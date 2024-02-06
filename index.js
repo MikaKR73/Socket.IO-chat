55a8769ecc389ab171b3f4892472d80b7c7cb75a
Mika Rasinmäki <mika.rasinmaki@gmail.com>
2024-02-06 10:43:30 +0200

    Opelta kopioitu koodi jota käydään tunilla läpi

---
+++ index.js
@@ -0,0 +1,27 @@
+// BACK END
+const express = require('express');
+const { createServer } = require('http');
+const { join } = require('path');
+const { Server } = require('socket.io');
+const PORT = process.env.PORT || 3000; // Mahdollistaa julkaisualustan tarjoaman portin käytön
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// Tässä ei nyt käytetäkään express staticia vaan lähetetään html manuaalisesti
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+io.on('connection', (socket) => {
+
+  // Kun palvelin vastaanottaa viestin se emitoi sen kaikille clienteille heti
+socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+});
+});
+
+server.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
